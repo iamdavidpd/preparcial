b26fd8393b87786682101de7f857f0f68fc76943
iamdavidpd <jd.perezd1@uniandes.edu.co>
2025-11-20 15:00:41 -0500

    feat: implement role-based access control with RoleGuard and roles decorator

---
+++ src/auth/roles/roles.guard.ts
@@ -0,0 +1,26 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { ROLES_KEY } from "./roles.decorator";
+
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+    constructor(
+        private reflector: Reflector
+    ){}
+
+    canActivate(context: ExecutionContext): boolean {
+        const requiredRoles = this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+            context.getHandler(),
+            context.getClass()
+        ]);
+        console.log('requiredRoles', requiredRoles);
+        if(!requiredRoles) return true;
+
+        const { user } = context.switchToHttp().getRequest();
+        console.log(user);
+
+        return requiredRoles.some((role) => user.role?.includes(role));
+    }
+}
