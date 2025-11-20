b0daf629fdbd08d35146956635affaf9bb39b26d
iamdavidpd <jd.perezd1@uniandes.edu.co>
2025-11-20 06:34:41 -0500

    feat: add ConfigModule and controllers for users and roles, update database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { RolesModule } from './roles/roles.module';
+import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersEntity } from './users/users.entity';
 import { RolesEntity } from './roles/roles.entity';
 
 @Module({
   imports: [UsersModule, RolesModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env'
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'preparcial',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       entities: [UsersEntity, RolesEntity],
       dropSchema: true,
       synchronize: true,
--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesEntity } from './roles.entity';
+import { RolesController } from './roles.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([RolesEntity])],
-  providers: [RolesService]
+  providers: [RolesService],
+  controllers: [RolesController]
 })
 export class RolesModule {}
